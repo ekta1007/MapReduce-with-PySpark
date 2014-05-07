d21724d688c8b1e6a36e30a4685be86589a0283f
Ekta1007 <ekta1007@gmail.com>
2014-05-07 13:45:15 +0530

    commiting

---
+++ hello_world.py
@@ -0,0 +1,26 @@
+import sys
+import json
+import os
+from pyspark import SparkContext
+
+# json = list of dict of dict in this input
+def time_day(json):
+	time_day="N/A"
+	if json.has_key('source'):
+		source = json['source']
+		if source.has_key('istdt') : #and source['app_id'] != "":
+			time_day = int(source['istdt'].split()[-1].split(":")[0])
+	return (source['istdt'], time_day)
+
+if __name__ == "__main__":
+	file_name = sys.argv[1]
+	if not os.path.isfile(file_name) or not os.access(file_name, os.R_OK):
+		print >> sys.stderr, "File '%s' does not exist or isn't accessible" % (file_name)
+		exit(-1)
+	sc = SparkContext("local[4]", "mopub_adlogs")
+	lines = sc.textFile(file_name)
+	list_dict = lines.map(lambda x: json.loads(x)).map(time_day)
+	list_dict.collect() # will list all the required data that you wanted to convert 
+		
+# hello world in spark
+
